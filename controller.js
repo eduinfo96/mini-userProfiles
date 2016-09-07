7419bf204a874c1da15d20ad2d9676e462fe6142
Theo Beck <eduinfo96@yahoo.com>
2016-09-07 12:34:47 -0500

    2.0 Finished

--- controller.js
+++ controller.js
@@ -1,8 +1,12 @@
 angular.module('userProfiles').controller( 'MainController' , function($scope, mainService){
   $scope.getUsers = function() {
-    $scope.users = mainService.getUsers();
+    mainService.getUsers().then( function( result ) {
+      $scope.users = result;
+
+
+    })
   }
 
   $scope.getUsers();
-  $scope.toggleFavorite = mainService.toggleFavorite;
+  // $scope.toggleFavorite = mainService.toggleFavorite;
 })
